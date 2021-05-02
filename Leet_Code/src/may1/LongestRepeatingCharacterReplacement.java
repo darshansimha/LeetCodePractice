5670503961ed628daeb6e9da6a7b6d8534ef42c1
Darshan Simha U <simha@adobe.com>
2021-05-02 16:50:26 +0530

    Permutation In A String

---
+++ Leet_Code/src/may1/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,20 @@
+package may1;
+
+public class LongestRepeatingCharacterReplacement {
+	public int characterReplacement(String s, int k) {
+		int result = 0;
+		 
+		
+		for(int i = 0; i < s.length(); i++) {
+			
+		}
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
