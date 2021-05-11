bc4b631f6d78e8f5202db222330cbdf649d16400
Darshan Simha U <simha@adobe.com>
2021-05-11 06:28:45 +0530

    Dp and Tree Problems

---
+++ Leet Code Topic Wise Prepration/src/Trees/TreeNode.java
@@ -0,0 +1,21 @@
+package Trees;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode() {
+	}
+	TreeNode(int val) {
+		this.val = val;
+	}
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+	@Override
+	public String toString(){
+		return "Val : " + val + " Left : " + left != null ? String.valueOf(left.val) : "" + " Right : " + right != null ? String.valueOf(right.val) : "";
+	}
+}
