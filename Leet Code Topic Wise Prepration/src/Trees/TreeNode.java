b7457a58d0c06214058026647b94a6fb4dd6eb36
Darshan Simha U <simha@adobe.com>
2021-05-12 21:47:56 +0530

    Search In a rotated sorted array

--- Leet Code Topic Wise Prepration/src/Trees/TreeNode.java
+++ Leet Code Topic Wise Prepration/src/Trees/TreeNode.java
@@ -4,18 +4,37 @@ public class TreeNode {
 	int val;
 	TreeNode left;
 	TreeNode right;
+
 	TreeNode() {
 	}
+
 	TreeNode(int val) {
 		this.val = val;
 	}
+
 	TreeNode(int val, TreeNode left, TreeNode right) {
 		this.val = val;
 		this.left = left;
 		this.right = right;
 	}
+
 	@Override
-	public String toString(){
-		return "Val : " + val + " Left : " + left != null ? String.valueOf(left.val) : "" + " Right : " + right != null ? String.valueOf(right.val) : "";
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+
+		s.append("Val : " + val);
+		if (left != null) {
+			s.append(" left : " + left.val);
+		} else {
+			s.append(" left : NULL");
+		}
+
+		if (left != null) {
+			s.append(" right : " + right.val);
+		} else {
+			s.append(" right : NULL");
+		}
+
+		return s.toString();
 	}
 }
